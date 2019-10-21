76b704bec981c9c482e04b2deac8df6389013063
Solomon Grundy <ngwtt@yahoo.com>
2019-10-20 20:55:20 -0600

    added functions to get physical address from user

--- WebScraperTut/WebScraperTut.py
+++ WebScraperTut/WebScraperTut.py
@@ -1,1 +1,26 @@
+#! python3
 
+
+import webbrowser, sys, pyperclip
+
+
+class utils :
+
+    def getCmdArgs():
+        address = pyperclip.paste()
+        if (len(sys.argv) > 1 and not address):
+            #get cmd line args
+            address = ' '.join(sys.argv[1:])         
+
+        return address
+
+    def openMap(urladdy):
+        webbrowser.open('https://www.google.com/maps/place' + urladdy)
+
+    def main():
+        testOuter = utils.getCmdArgs()
+        utils.openMap(testOuter)
+
+
+
+utils.main()
